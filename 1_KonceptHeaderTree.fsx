25febe266d11ec0d847ed3e09cb1da2762d6f3af
Daniel Ljunghill <danielljunghill@hotmail.com>
2021-04-19 20:06:16 +0200

    save work on cubeHeaderTree

--- 1_KonceptHeaderTree.fsx
+++ 1_KonceptHeaderTree.fsx
@@ -85,7 +85,7 @@ type CubeHeader  =
            Attributes:  String List
            Name: String
            IsSelected: bool
-           indent: Indent option
+           NotImplementedException: Indent option
     }
 
 let rec calculateSpanForKonceptTree (koncept: DimensionalKoncept) =
@@ -106,7 +106,19 @@ let rec calculateSpanForKonceptTree (koncept: DimensionalKoncept) =
 
 
 
-let rec calculateKontextAsTree  (parent: CubeAbstractRow option) (kontexts: DimensionalKoncept list) =
+let createHeader (columSpan: HorizontalSpan)  (column: HorizontalStart) (rowSpan: VerticalSpan) (row: VerticalStart) () (name: String) =
+    {
+           Column = horizontalStartToStart column
+           ColumnSpan = horizontalSpanToSpan columSpan
+           Row =  verticalStartToStart row
+           RowSpan = verticalSpanToSpan rowSpan
+           Attributes = []
+           Name = name
+           IsSelected = false
+           Indent = None
+    }
+
+let rec calculateCubeRowsForTree (parent: CubeAbstractRow option) (kontexts: DimensionalKoncept list) =
    match kontexts with
    | [] -> []
    | head :: tail ->
@@ -125,24 +137,88 @@ let rec calculateKontextAsTree  (parent: CubeAbstractRow option) (kontexts: Dime
                 //   let area = calculateSubLevelArea direction area head
                   kontextHeader (Some abstractRow) head
                   //calculate pa for tail
-                  @ calculateKontextAsTree state tail
+                  @ calculateCubeRowsForTree state tail
           | DimensionalValue vk ->
                 [ 
                     vk 
                     |> CubeAbstractRow.foldValue state 
-                    |> ValueRow
+                    |> ValueRow   
                    ]
 
-
-      let span = head |> calculateSpanForKonceptTree |> Span
-      let newArea =  createAreaForKoncept direction span pa
-      printfn "newArea %A Header %A" newArea head
-      kontextHeader newArea head
-      //calculate pa for tail
-      @ calculateKontextAsTree direction newArea tail
+      kontextHeader parent head
+      @ calculateCubeRowsForTree parent tail
 
 
+module CubeHeader =
+    let newAbstractChild  (ak: AbstractKoncept) (cubeHeader: CubeHeader) =
+        //add columns + 1
+        { cubeHeader with Name =  abstractKonceptNameToString ak.Name }
 
+    let newAbstractSibling  (ak: AbstractKoncept) (cubeHeader: CubeHeader) =
+        //add columns + 1
+        { cubeHeader with Name =  abstractKonceptNameToString ak.Name; Row = Area.startIncrement cubeHeader.Row  }
+
+    let newValueChild  (vk: ValueKoncept) (cubeHeader: CubeHeader) =
+        //add columns + 1
+        { cubeHeader with Name =  valueKonceptNameToString vk.Name }
+
+    let newValueSibling  (vk: ValueKoncept) (cubeHeader: CubeHeader) =
+        //add columns + 1
+        { cubeHeader with Name =  valueKonceptNameToString vk.Name; Row = Area.startIncrement cubeHeader.Row  }
+
+    let newHeader f defaultHeader (cubeHeader: CubeHeader option) =
+        match cubeHeader with
+        | Some header -> f defaultHeader cubeHeader
+        | None -> defaultHeader
+
+let rec calculateCubeHeaderForTree (parent: CubeHeader option) (kontexts: DimensionalKoncept list) =
+   match kontexts with
+   | [] -> []
+   | head :: tail ->
+
+      let rec kontextHeader (state: Start) (kontext: DimensionalKoncept) =
+          match kontext with
+          | DimensionalAbstract (ak, koncepts) ->
+
+               let abstractRow = ak |> CubeAbstractRow.foldAbstract state
+               [ CubeRow.abstractRow ak ]
+               @
+               match koncepts with
+               | [] -> [ abstractRow |> AbstractRow ]
+               | head :: tail ->
+                  //calculate pa för head
+                //   let area = calculateSubLevelArea direction area head
+                  kontextHeader (Some abstractRow) head
+                  //calculate pa for tail
+                  @ calculateCubeRowsForTree state tail
+          | DimensionalValue vk ->
+                [ 
+                    vk 
+                    |> CubeAbstractRow.foldValue state 
+                    |> ValueRow   
+                   ]
+
+      kontextHeader parent head
+      @ calculateCubeRowsForTree parent tail
+
+let ca name koncepts =  DimensionalAbstract ((name|> createAbstractKoncept), koncepts)
+let ca1 name koncept =  DimensionalAbstract ((name|> createAbstractKoncept), [ koncept ])
+let cv name = DimensionalValue (name |> createValueKoncept )
+let ca2 name =  ca name []
+let empty = ca "e" []
+let t = ca1 "1" (ca1 "2" (ca1 "3" (ca "4" [] )))
+let t' = ca1 "1" (ca1 "2" (ca1 "3" (ca "4" [t ] )))
+let t2 = ca "head" [ ca2 "first"; ca2 "second"; cv "third"]
+let t3 = ca "head" [ t2 ]
+let testKoncept = 
+    DimensionalAbstract ("Abstract1"|> createAbstractKoncept, [DimensionalAbstract ("Abstract2"|> createAbstractKoncept  , [ "Value1" |> createValueKoncept |> DimensionalValue ]); ])
+let testKoncept1 = 
+    DimensionalAbstract ("Abstract1"|> createAbstractKoncept, [DimensionalAbstract ("Abstract2"|> createAbstractKoncept , [ "Value1" |>  createValueKoncept |> DimensionalValue;testKoncept ;testKoncept]); ])
+let testKoncept2 = 
+    DimensionalAbstract ("Abstract1"|> createAbstractKoncept, [DimensionalAbstract ("Abstract2"|> createAbstractKoncept , [ "Value1" |>  createValueKoncept |> DimensionalValue ; testKoncept1]); ])
+
+
+let rows1 =  calculateCubeRowsForTree None [ testKoncept ; testKoncept2 ;[] |> ca "Abstract1X"  ] 
 // let rec calculateKontextAsTree  (parent: CubeAbstractRow option) (kontexts: DimensionalKoncept list) =
 //    match kontexts with
 //    | [] -> []
